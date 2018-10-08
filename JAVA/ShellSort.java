2b7851ccce1bfa0a0f2987771f19ad7872af91e0
Kostiantyn Lelikov <kostiantynlelikov@iMac-Kostiantyn.local>
2018-10-08 22:01:45 +0300

    Add ShellSort

--- JAVA/ShellSort.java
+++ JAVA/ShellSort.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class ShellSort {
+    public static int[] sort(int arr[]) {
+        int n = arr.length;
+        for (int gap = n/2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i += 1) {
+                int tmp = arr[i];
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > tmp; j -= gap)
+                    arr[j] = arr[j - gap];
+                arr[j] = tmp;
+            }
+        }
+        return arr;
+    }
 }
